c5930163367522c13bbe17d5c95d9ba8084612bc
Mohamed Ali <a.mohamed.cali@gmail.com>
2025-03-18 22:02:45 +0100

    Store the plant image

--- app/new.tsx
+++ app/new.tsx
@@ -42,7 +42,7 @@ export default function NewScreen() {
       );
     }
 
-    addPlant(name, Number(days));
+    addPlant(name, Number(days), imageUri);
     router.navigate("/");
 
     console.log("Adding plant", name, days);
